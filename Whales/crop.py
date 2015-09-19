9addb6050001d543464ee1b1f677fce190a98f7b
fierval <fierval@gmail.com>
2015-09-18 20:34:55 -0700

    Cropping

--- Whales/crop.py
+++ Whales/crop.py
@@ -6,9 +6,6 @@ import pandas as pd
 from kobra.imaging import *
 from kobra.dr.retina import display_contours
 
-def saturate (v):
-    return map(lambda a: min(max(round(a), 0), 255), v)
-
 # given a list of images, create masks for them
 def get_masks(images):
     def mask(im):
@@ -49,44 +46,71 @@ def pre(img):
 
     return img_shift
 
+# preprocess ref image
+ref_im_pre = pre(ref_image)
+
 def kmeans(image):
     Z = image.reshape((-1,3))
     Z = np.float32(Z)
 
     # define criteria, number of clusters(K) and apply kmeans()
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-    K = 2
+    K = 3
     ret,label,center=cv2.kmeans(Z,K,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
 
     # Now convert back into uint8, and make original image
     center = np.uint8(center)
     res = center[label.flatten()]
     res2 = res.reshape((image.shape))
-    return res2
+    return res2, center, label.flatten().reshape((image[:,:,0].shape))
 
-def process(image_titles, images):
-
-    ims_pre = map(pre, images)
-
-    # get the masks
+# takes pre-processed images
+# prep
+def process(ims_pre):
+    # get the masks (just stubs)
     masks = np.array(get_masks(ims_pre))
-
-    ref_im_pre = pre(ref_image)
 
     refMask = get_masks([ref_im_pre])
     refHist = calc_hist([ref_im_pre], refMask)
 
     histSpec = histogram_specification(refHist, ims_pre, masks)
-
-    #cdfs = calc_hist(ims_pre, masks)
-
-    #plot_hist(cdfs[0][1], "g")
     kmeans_imgs = map(kmeans, histSpec)
 
-    # mask off the background
-    show_images(kmeans_imgs, titles = image_titles, scale = 0.9)
-
+    #show_images(map(lambda x: x[0], kmeans_imgs), scale = 0.9)
     return kmeans_imgs
 
-def contours(imgs):
+def crop(images):
+    ims_pre = map(pre, images)
+    ims = process(ims_pre)
 
+    for j, im_tuple in enumerate(ims):
+        proc_im = im_tuple[0]
+        center = im_tuple[1]
+        labels = im_tuple[2]
+
+        lab_quants = [labels[labels == i].shape[0] for i in range(0,3)]
+        max_ind = argmax(lab_quants)
+        min_ind = argmin(lab_quants)
+
+        middle = filter(lambda r: r != max_ind and r != min_ind, range(0, 3))[0]
+
+        lower = center[argmax(lab_quants)]
+        upper = center[middle]
+        mask = cv2.inRange(proc_im, lower, upper)
+
+        _, cnts, _ = cv2.findContours(mask.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        # get 10 largest areas, except for the last one
+        areas = map(cv2.contourArea, cnts)
+        contour_areas_sorted = argsort(areas)[:-1]
+
+        cnts = [cnts[i] for i in contour_areas_sorted[::-1][:10]]
+        # bounding rectangle for cropping
+        x, y, w, h = cv2.boundingRect(np.vstack(cnts))
+
+        # good for debugging
+        #out_rect = cv2.rectangle(proc_im.copy(), (x, y), (x + w, y + h), (0, 255, 0), 2)
+        #plt.imshow(out_rect); plt.show()
+
+        # now crop it
+        yield ims_pre[j][y:y+h, x:x+w, :] 

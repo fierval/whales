50e258642435c67383d072475510ea7beeb639ad
Boris Kogan <bernole@hotmail.com>
2015-09-22 12:50:22 -0700

    training preparation

--- Whales/Features/split_whales.py
+++ Whales/Features/split_whales.py
@@ -10,6 +10,8 @@ labels_file = "/Kaggle/whales/train.csv"
 inp_path = "/Kaggle/whales/cropped"
 out_path = "/Kaggle/whales/train"
 test_path = "/Kaggle/whales/test"
+train_path = "/Kaggle/whales/kerano"
+labels_map = "/Kaggle/whales/labels_map.csv"
 
 def copy_files_to_label_dirs(inp_path, out_path, labels_file):
     prep_out_path(out_path)
@@ -46,5 +48,26 @@ def copy_test_files(inp_path, test_path, labels_file):
     for f in test_files:
         copy(path.join(inp_path, f), path.join(test_path, f))
 
+def copy_train_files(inp_path, train_path, labels_file):
+    prep_out_path(train_path)
+    labels = pd.read_csv(labels_file)
+    files_names = set(labels[labels.columns[0]].as_matrix())
+    all_files = set(os.listdir(inp_path))
+    train_files = all_files.intersection(files_names)
+
+    for f in train_files:
+        copy(path.join(inp_path, f), path.join(train_path, f))
+
+def map_labels(labels_file, labels_map):
+    labels = pd.read_csv(labels_file)
+    labels = labels[labels.columns[1]]
+    unique_labs = pd.unique(labels)
+    ld = {value: key for key, value in enumerate(unique_labs)}
+    ds = pd.DataFrame.from_dict(ld, orient='index')
+    ds.to_csv(labels_map, header = False)
+
 copy_files_to_label_dirs(inp_path, out_path, labels_file)
 copy_test_files(inp_path, test_path, labels_file)
+copy_train_files(inp_path, train_path, labels_file)
+
+map_labels(labels_file, labels_map)
---
+++ Whales/Learning/cnn_keras.py
@@ -0,0 +1,44 @@
+import numpy as np
+import pandas as pd
+import os
+from os import path
+import matplotlib.pylab as plt
+
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
+from keras.optimizers import SGD
+
+out_path = "/Kaggle/whales/train"
+test_path = "/Kaggle/whales/test"
+train_path = "/Kaggle/whales/kerano"
+labels_map = "/Kaggle/whales/labels_map.csv"
+
+def train(X_train, Y_train):
+    model = Sequential()
+    model.add(Convolution2D(32, 3, 3, 3, border_mode='full')) 
+    model.add(Activation('relu'))
+    model.add(Convolution2D(32, 32, 3, 3))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(poolsize=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Convolution2D(64, 32, 3, 3, border_mode='full')) 
+    model.add(Activation('relu'))
+    model.add(Convolution2D(64, 64, 3, 3)) 
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(poolsize=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Flatten())
+    model.add(Dense(64*8*8, 1000))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+
+    model.add(Dense(1000, 447))
+    model.add(Activation('softmax'))
+
+    sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
+    model.compile(loss='categorical_crossentropy', optimizer=sgd)
+
+    model.fit(X_train, Y_train, batch_size=10, nb_epoch=1)

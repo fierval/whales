26f866e71a42ac1a5aa379a9d3ea0e5a31b0152d
Boris Kogan <bernole@hotmail.com>
2015-09-22 18:28:19 -0700

    data loading and training

--- Whales/Learning/cnn_keras.py
+++ Whales/Learning/cnn_keras.py
@@ -4,17 +4,29 @@ import os
 from os import path
 import matplotlib.pylab as plt
 import cv2
-from BatchGenerator import BatchGenerator
+from BatchGenerator import DataSetLoader
 
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation, Flatten
 from keras.layers.convolutional import Convolution2D, MaxPooling2D
 from keras.optimizers import SGD
+from keras.preprocessing.image import ImageDataGenerator
 
-train_path = "/Kaggle/whales/train"
+train_path = "/Kaggle/whales/kerano"
+labels_file = "/Kaggle/whales/train.csv"
 labels_map = "/Kaggle/whales/labels_map.csv"
 
-def train(X_train, Y_train):
+def train(train_path, labels_file, labels_map):
+    datagen = ImageDataGenerator(
+        featurewise_center=True,
+        featurewise_std_normalization=True,
+        rotation_range=20,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        horizontal_flip=True)
+
+    dsl = DataSetLoader(train_path, labels_file, labels_map)
+
     model = Sequential()
     model.add(Convolution2D(32, 3, 3, 3, border_mode='full')) 
     model.add(Activation('relu'))

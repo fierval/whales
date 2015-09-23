a7a319aaa0881c55ed59f12bc42373f4c1a010b8
fierval <fierval@gmail.com>
2015-09-23 08:56:55 -0700

    Label map that coincides with submission file

--- Whales/Features/split_whales.py
+++ Whales/Features/split_whales.py
@@ -6,7 +6,8 @@ from kobra.tr_utils import prep_out_path
 import shutil
 from shutil import copy
 
-labels_file = "/Kaggle/whales/train.csv"    
+labels_file = "/Kaggle/whales/train.csv"
+sample_submission = "/Kaggle/whales/sample_submission.csv"
 inp_path = "/Kaggle/whales/cropped"
 out_path = "/Kaggle/whales/train"
 test_path = "/Kaggle/whales/test"
@@ -58,11 +59,9 @@ def copy_train_files(inp_path, train_path, labels_file):
     for f in train_files:
         copy(path.join(inp_path, f), path.join(train_path, f))
 
-def map_labels(labels_file, labels_map):
-    labels = pd.read_csv(labels_file)
-    labels = labels[labels.columns[1]]
-    unique_labs = pd.unique(labels)
-    ld = {value: key for key, value in enumerate(unique_labs)}
+def map_labels(sample_submission, labels_map):
+    labels = pd.read_csv(sample_submission).columns[1:]
+    ld = {value: key for key, value in enumerate(list(labels))}
     ds = pd.DataFrame.from_dict(ld, orient='index')
     ds.to_csv(labels_map, header = 0)
 
@@ -70,4 +69,4 @@ copy_files_to_label_dirs(inp_path, out_path, labels_file)
 copy_test_files(inp_path, test_path, labels_file)
 copy_train_files(inp_path, train_path, labels_file)
 
-map_labels(labels_file, labels_map)
+map_labels(sample_submission, labels_map)

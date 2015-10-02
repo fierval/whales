b9929381443c477692e4767e83dde8a71787f6d9
Boris Kogan <bernole@hotmail.com>
2015-10-02 16:58:48 -0700

    New model

--- Whales/Learning/cnn_keras.py
+++ Whales/Learning/cnn_keras.py
@@ -13,124 +13,143 @@ from sklearn.utils import shuffle as sk_shuffle
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation, Flatten
 from keras.layers.convolutional import Convolution2D, MaxPooling2D
-from keras.layers.normalization import BatchNormalization
-from keras.optimizers import SGD
+from keras.layers.normalization import BatchNormalization, LRN2D
+from keras.optimizers import SGD, Adagrad
 from keras.preprocessing.image import ImageDataGenerator
 #from keras.utils.dot_utils import Grapher
 from keras.utils import generic_utils
 
-train_path = "/Kaggle/whales/augmented"
-val_path = "/kaggle/whales/kerano"
+train_path = "/Kaggle/whales/kerano"
 labels_file = "/Kaggle/whales/train.csv"
 labels_map = "/Kaggle/whales/labels_map.csv"
-
-#dsl = DataSetLoader(val_path, labels_file, labels_map)
-#X_train, Y_train, X_test, Y_test = dsl.get_fraction(.8)
+labels_file = "/Kaggle/whales/train.csv"
 
 model = Sequential()
-model.add(BatchNormalization((3, 256, 256)))
-model.add(Convolution2D(32, 3, 3, 3, border_mode='full')) 
+#model.add(BatchNormalization((3, 384, 384)))
+model.add(Convolution2D(32, 3, 3, 3, border_mode='full', init='glorot_normal')) 
 model.add(Activation('relu'))
-#model.add(BatchNormalization((32, 262, 262)))
-model.add(Convolution2D(32, 32, 3, 3))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(poolsize=(2, 2)))
-model.add(Dropout(0.25))
-
-model.add(Convolution2D(64, 32, 3, 3, border_mode='full')) 
-model.add(Activation('relu'))
-#model.add(BatchNormalization((64, 135, 135)))
-model.add(Convolution2D(64, 64, 5, 5)) 
+#model.add(BatchNormalization((32, 390, 390)))
+model.add(Convolution2D(32, 32, 7, 7))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(poolsize=(2, 2)))
 model.add(Dropout(0.25))
 
-model.add(Convolution2D(128, 64, 3, 3, border_mode='full')) 
+model.add(Convolution2D(64, 32, 5, 5, border_mode='full', init='glorot_normal')) 
 model.add(Activation('relu'))
-#model.add(BatchNormalization((128, 69, 69)))
+#model.add(BatchNormalization((64, 199, 199)))
+model.add(Convolution2D(64, 64, 3, 3)) 
+model.add(Activation('relu'))
+model.add(MaxPooling2D(poolsize=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Convolution2D(96, 64, 3, 3, border_mode='full', init='glorot_normal')) 
+model.add(Activation('relu'))
+#model.add(BatchNormalization((96, 69, 69)))
+model.add(Convolution2D(96, 96, 3, 3)) 
+model.add(Activation('relu'))
+model.add(MaxPooling2D(poolsize=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Convolution2D(128, 96, 3, 3, border_mode='full', init='glorot_normal')) 
+model.add(Activation('relu'))
+#model.add(BatchNormalization((128, 36, 36)))
 model.add(Convolution2D(128, 128, 3, 3)) 
 model.add(Activation('relu'))
 model.add(MaxPooling2D(poolsize=(2, 2)))
 model.add(Dropout(0.25))
 
-model.add(Convolution2D(256, 128, 3, 3, border_mode='full')) 
+model.add(Convolution2D(160, 128, 3, 3, border_mode='full', init='glorot_normal')) 
 model.add(Activation('relu'))
-#model.add(BatchNormalization((256, 36, 36)))
-model.add(Convolution2D(256, 256, 3, 3)) 
-model.add(Activation('relu'))
-model.add(MaxPooling2D(poolsize=(2, 2)))
-model.add(Dropout(0.25))
-
-model.add(Convolution2D(512, 256, 3, 3, border_mode='full')) 
-model.add(Activation('relu'))
-#model.add(BatchNormalization((512, 20, 20)))
-model.add(Convolution2D(512, 512, 3, 3)) 
+#model.add(BatchNormalization((256, 20, 20)))
+model.add(Convolution2D(160, 160, 3, 3)) 
 model.add(Activation('relu'))
 model.add(MaxPooling2D(poolsize=(2, 2)))
 model.add(Dropout(0.25))
 
-model.add(Convolution2D(1024, 512, 3, 3, border_mode='full')) 
+model.add(Convolution2D(192, 160, 3, 3, border_mode='full')) 
 model.add(Activation('relu'))
-#model.add(BatchNormalization((512, 20, 20)))
-model.add(Convolution2D(1024, 1024, 3, 3)) 
+#model.add(BatchNormalization((512, 12, 12)))
+model.add(Convolution2D(192, 192, 3, 3)) 
+model.add(Activation('relu'))
+model.add(MaxPooling2D(poolsize=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Convolution2D(224, 192, 3, 3, border_mode='full')) 
+model.add(Activation('relu'))
+model.add(BatchNormalization((224, 6, 6)))
+model.add(Convolution2D(224, 224, 3, 3)) 
 model.add(Activation('relu'))
 model.add(MaxPooling2D(poolsize=(2, 2)))
 model.add(Dropout(0.25))
     
 model.add(Flatten())
-model.add(Dense(9216, 4000))
-model.add(Activation('relu'))
-model.add(BatchNormalization((4000,)))
-model.add(Dropout(0.5))
+#model.add(Dense(25600, 4000))
+#model.add(Activation('relu'))
+##model.add(BatchNormalization((4000,)))
+#model.add(Dropout(0.5))
 
-model.add(Dense(4000, 2000))
-model.add(Activation('relu'))
-model.add(BatchNormalization((2000,)))
-model.add(Dropout(0.5))
+#model.add(Dense(4000, 2000))
+#model.add(Activation('relu'))
+#model.add(BatchNormalization((2000,)))
+#model.add(Dropout(0.5))
 
-model.add(Dense(2000, 1000))
-model.add(Activation('relu'))
-model.add(BatchNormalization((1000,)))
-model.add(Dropout(0.5))
+#model.add(Dense(3072, 2000))
+#model.add(Activation('relu'))
+#model.add(BatchNormalization((2000,)))
+#model.add(Dropout(0.5))
 
-model.add(Dense(1000, 447))
+#model.add(Dense(2000, 1000))
+#model.add(Activation('relu'))
+#model.add(BatchNormalization((1000,)))
+#model.add(Dropout(0.5))
+
+model.add(Dense(896, 447))
 model.add(Activation('softmax'))
 
 sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
-model.compile(loss='categorical_crossentropy', optimizer=sgd)
+model.compile(loss='categorical_crossentropy', optimizer=Adagrad())
 
 #grapher = Grapher()
 #grapher.plot(model, "/temp/graph.png")
 
-nb_epoch = 5
-batch_size = 3000
-nb_samples = 30
+nb_epoch = 2
+batch_size = 300
+nb_samples = 2
 
 from kobra.tr_utils import time_now_str
 print("Start time: " + time_now_str())
 
 #x_val, y_val = BatchGenerator(train_path, labels_map, batch_size).get_val()
 
-for e in range(nb_epoch):
-    print("Epoch %d" % e)
-    batches = BatchGenerator(train_path, labels_map, batch_size)
+dsl = DataSetLoader(train_path, labels_file, labels_map)
+imgen = ImageDataGenerator()
+imgen.fit(dsl.X_train)
 
-    progbar = generic_utils.Progbar(batches.total)
-    for x_train, y_train in batches:
+X_train = dsl.X_train - imgen.mean
+X_train = dsl.X_train / imgen.std
+
+model.fit(X_train, dsl.Y_train, batch_size=5, nb_epoch=1, validation_split=0.1)
+
+#for e in range(nb_epoch):
+#    print("Epoch %d" % e)
+#    batches = BatchGenerator(train_path, labels_file, labels_map, batch_size)
+
+#    progbar = generic_utils.Progbar(batches.total)
+#    for x_train, y_train in batches:
                
-        iterations = x_train.shape[0] / nb_samples if x_train.shape[0] % nb_samples == 0 else (x_train.shape[0] + nb_samples) / nb_samples
+#        iterations = x_train.shape[0] / nb_samples if x_train.shape[0] % nb_samples == 0 else (x_train.shape[0] + nb_samples) / nb_samples
         
-        for i in range(iterations):
-            X_batch = x_train[i * nb_samples : (i + 1) * nb_samples]
-            Y_batch = y_train[i * nb_samples : (i + 1) * nb_samples]
-            loss = model.train_on_batch(X_batch, Y_batch)
-            progbar.add(X_batch.shape[0], values= [("train loss", loss)])
+#        for i in range(iterations):
+#            X_batch = x_train[i * nb_samples : (i + 1) * nb_samples]
+#            Y_batch = y_train[i * nb_samples : (i + 1) * nb_samples]
+#            loss = model.train_on_batch(X_batch, Y_batch)
+#            progbar.add(X_batch.shape[0], values= [("train loss", loss)])
 
-    loss, accuracy = model.evaluate(x_val, y_val, show_accuracy=True)
-    print ("Validation: loss - {0}, accuracy - {1}".format(loss, accuracy))
+#    loss, accuracy = model.evaluate(x_val, y_val, show_accuracy=True)
+#    print ("Validation: loss - {0}, accuracy - {1}".format(loss, accuracy))
          
 print("End time: " + time_now_str())
 
 json_string = model.to_json()
-open('/users/boris/dropbox/kaggle/whales/models/model_1.json', 'w').write(json_string)
-model.save_weights('/users/boris/dropbox/kaggle/whales/models/weights_1.h5', overwrite=True)
+open('/users/boris/dropbox/kaggle/whales/models/model_2.json', 'w').write(json_string)
+model.save_weights('/users/boris/dropbox/kaggle/whales/models/weights_2.h5', overwrite=True)
--- Whales/Learning/submission.py
+++ Whales/Learning/submission.py
@@ -2,25 +2,35 @@
 from os import path
 import cv2
 import numpy as np
-
+from BatchGenerator import DataSetLoader
+from keras.models import Sequential
 from keras.models import model_from_json
+from keras.preprocessing.image import ImageDataGenerator
 
 test_path = "/Kaggle/whales/test"
+train_path = "/Kaggle/whales/kerano"
 labels_file = "/Kaggle/whales/train.csv"
 labels_map = "/Kaggle/whales/labels_map.csv"
 models_path = "/users/boris/dropbox/kaggle/whales/models"
 sample_submission = "/Kaggle/whales/sample_submission.csv"
 
-json_path = path.join(models_path, "model_1.json")
+json_path = path.join(models_path, "model_2.json")
 model = model_from_json(open(json_path).read())
-model.load_weights(path.join(models_path, "weights_1.h5"))
+model.load_weights(path.join(models_path, "weights_2.h5"))
 
 ss = pd.read_csv(sample_submission)
 images = pd.DataFrame(ss['Image'])
 ims = np.array(map(cv2.imread, np.array(map(lambda im_f: path.join(test_path, im_f), images.values)).flatten())).transpose(0, 3, 1, 2)
 
-y_train = model.predict(ims)
-labels = pd.DataFrame(y_train, columns=ss.columns[1:])
+dsl = DataSetLoader(train_path, labels_file, labels_map)
+imgen = ImageDataGenerator()
+imgen.fit(dsl.X_train)
+
+X_test = ims - imgen.mean
+X_test = X_test / imgen.std
+
+Y_test = model.predict(X_test, batch_size=5)
+labels = pd.DataFrame(Y_test, columns=ss.columns[1:])
 preds = images.join(labels)
 
-preds.to_csv("/kaggle/whales/submissions/sub1.csv", index=False)
+preds.to_csv("/kaggle/whales/submissions/sub2.csv", index=False)

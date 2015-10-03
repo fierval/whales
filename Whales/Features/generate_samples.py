c51232227956c04433ebc0106b31a16494d49362
Boris Kogan <bernole@hotmail.com>
2015-10-03 16:27:03 -0700

    augmentation

--- Whales/Features/generate_samples.py
+++ Whales/Features/generate_samples.py
@@ -11,6 +11,7 @@ import cv2
 from kobra.tr_utils import time_now_str, prep_out_path
 import shutil
 from scipy import ndimage, misc
+from keras.preprocessing.image import random_zoom
 
 root = "/Kaggle/whales/kerano"
 out_dir = "/kaggle/whales/augmented"
@@ -19,8 +20,11 @@ labels_file = "/kaggle/whales/train.csv"
 
 dirs = pd.read_csv(labels_map, header=None).icol(0).values
 labels = pd.read_csv(labels_file)
+rg = [1.5, 2]
+rotations = range(60, 180 + 60, 60)
+size = (256, 256)
 
-def generate_samples(dir):
+def generate_samples(dir, flip=False, rotate=True, zoom = False):
     files = labels[labels['whaleID'] == dir].icol(0).values
     for f in files:
         fl = path.join(root, f)
@@ -28,20 +32,30 @@ def generate_samples(dir):
         misc.imsave(path.join(out_dir, dir, f), im)
         new_name = path.splitext(path.split(fl)[1])[0]
 
-        for rot in range(60, 180 + 60, 60):
+        for rot in rotations:
             im_rot = ndimage.interpolation.rotate(im, rot, axes=(0,1), reshape=False, mode="nearest", cval=0.)
 
             save_name = "{0}-{1}".format(new_name, rot).replace(".", "-") + ".jpg"
 
             misc.imsave(path.join(out_dir, dir, save_name), im_rot)
-            print "rotated: {0} by {1}".format(f, rot)
+            #print "rotated: {0} by {1}".format(f, rot)
         
         # horizontal flip
-        im_rot = np.fliplr(im)
-        misc.imsave(path.join(out_dir, dir, new_name + "-hflip" + ".jpg"), im_rot)
-        im_rot = np.flipud(im)
-        misc.imsave(path.join(out_dir, dir, new_name + "-vflip" + ".jpg"), im_rot)
+        if flip:
+            im_rot = np.fliplr(im)
+            misc.imsave(path.join(out_dir, dir, new_name + "-hflip" + ".jpg"), im_rot)
+            im_rot = np.flipud(im)
+            misc.imsave(path.join(out_dir, dir, new_name + "-vflip" + ".jpg"), im_rot)
 
+        if zoom:
+            for z in rg:
+                im_zoomed = ndimage.zoom(im, (z, z, 1), order=0)
+                h, w = im_zoomed.shape[0 : 2]
+                dh  = size[0] * (z - 1) / 2
+                dw = size[1] * (z - 1) / 2
+
+                im_zoomed = misc.imresize(im_zoomed[dh:h-dh, dw:w-dw, :], size)
+                misc.imsave(path.join(out_dir, dir, new_name + "-z_{0}".format(z).replace(".", "-") + ".jpg"), im_zoomed)
     return dir, len(files)
 
 #for dir in dirs:
@@ -51,7 +65,7 @@ for dir in dirs:
     os.makedirs(path.join(out_dir, dir))
 
 dv = Client().load_balanced_view()
-fs = dv.map(generate_samples, np.array(dirs))
+fs = dv.map(generate_samples, np.array(dirs), False, True, True)
 print "Started: ", time_now_str()
 fs.wait()
 print "Finished: ", time_now_str()
